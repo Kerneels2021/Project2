40332771ee5eb68eac488b842e3591a190724580
Kerneels2021 <desmond@megabags.co.za>
2021-11-21 22:22:02 +0200

    Upload size

--- Project2/Controllers/ImageController.cs
+++ Project2/Controllers/ImageController.cs
@@ -27,6 +27,7 @@ namespace Project2.Controllers
 
         }
 
+       
         // Upload
         public ActionResult Upload()
         {
@@ -60,20 +61,7 @@ namespace Project2.Controllers
             return View();
         }
 
-        /*// POST: ImageController/Delete/5
-        [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }*/
+       
 
         // GET: ImageController/Edit/5
         public ActionResult Edit(int id)
@@ -81,94 +69,6 @@ namespace Project2.Controllers
             return View();
         }
 
-        /*// POST: ImageController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }*/
-
-        /* // GET: ImageController
-         public ActionResult Index()
-         {
-             return View();
-         }
-
-         // GET: ImageController/Details/5
-         public ActionResult Details(int id)
-         {
-             return View();
-         }
-
-         // GET: ImageController/Create
-         public ActionResult Create()
-         {
-             return View();
-         }
-
-         // POST: ImageController/Create
-         [HttpPost]
-         [ValidateAntiForgeryToken]
-         public ActionResult Create(IFormCollection collection)
-         {
-             try
-             {
-                 return RedirectToAction(nameof(Index));
-             }
-             catch
-             {
-                 return View();
-             }
-         }
-
-         // GET: ImageController/Edit/5
-         public ActionResult Edit(int id)
-         {
-             return View();
-         }
-
-         // POST: ImageController/Edit/5
-         [HttpPost]
-         [ValidateAntiForgeryToken]
-         public ActionResult Edit(int id, IFormCollection collection)
-         {
-             try
-             {
-                 return RedirectToAction(nameof(Index));
-             }
-             catch
-             {
-                 return View();
-             }
-         }
-
-         // GET: ImageController/Delete/5
-         public ActionResult Delete(int id)
-         {
-             return View();
-         }
-
-         // POST: ImageController/Delete/5
-         [HttpPost]
-         [ValidateAntiForgeryToken]
-         public ActionResult Delete(int id, IFormCollection collection)
-         {
-             try
-             {
-                 return RedirectToAction(nameof(Index));
-             }
-             catch
-             {
-                 return View();
-             }
-         }*/
+        
     }
 }
